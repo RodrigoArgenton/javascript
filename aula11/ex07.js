14bc5fd7e4c96277ac5318584a58c3a7bcfca51a
Rodrigo Argenton Barbosa <rodrigolk4321@gmail.com>
2023-09-25 16:02:39 -0400

    Aula 11 sobre condicionais simples e sua aplicação com html e css

---
+++ aula11/ex07.js
@@ -0,0 +1,7 @@
+var maxVel = 60
+var vel = 61
+console.log(`Você está com a a velocidade ${vel}km/h`)
+if (vel > maxVel){
+  console.log('Você ultrapassou o limete da velocidade permitida!')
+}
+console.log(`O limite de velocidade permitido para essa estrada é: ${maxVel}km/h`)
---
+++ aula11/ex08.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+console.log(`Você é natural do ${pais}`)
+if (pais == 'Brasil'){
+  console.log('Você é Brasileiro!')
+}
+else{
+  console.log('Você é estrangeiro!')
+}
