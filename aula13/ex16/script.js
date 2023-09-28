7bce234380785c056fff57ba08a50537091eb521
Rodrigo Argenton Barbosa <rodrigolk4321@gmail.com>
2023-09-28 15:22:51 -0400

    melhorias

--- aula13/ex16/script.js
+++ aula13/ex16/script.js
@@ -1,24 +1,39 @@
-function calcular(){
-  var ano = Number(window.document.getElementById('ano').value)
-  var sexo = window.document.querySelector("input[id='sexo']:checked").value
-  var anoAtual = new Date().getFullYear()
-  var idade = anoAtual - ano
-  var res = window.document.getElementById('resultado')
-
-  if (idade < 0 || ano == 0 ){
-    window.alert('Confira os dados novamente, ano se encontra invalido!')
-  }
-  else{
+function verificar(){
+  var ano = Number(new Date().getFullYear())
+  var fano = document.getElementById('txtano').value
+  var res = document.getElementById('res')
+  if (fano.length == 0 || Number(fano) > ano){
+    window.alert('Verifique os dados novamente!')
+  }else{
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano)
+    var genero = ''
     var img = document.createElement('img')
-    if(sexo == 'Feminino'){
-      if(idade >= 0  && idade <= 10){
-        img.src = 'imagem/criancaF.png'
-        res.appendChild(img)
+    img.setAttribute('id', 'foto')
+    if (fsex[0].checked){
+      genero = 'Homem'
+      if(idade >= 0 && idade < 10){
+        img.setAttribute('src', 'imagem/criancaM.png')
+      }else if (idade < 21){
+        img.setAttribute('src', 'imagem/adolescenteM.png')
+      }else if (idade < 50){
+        img.setAttribute('src', 'imagem/adultoM.png')
+      }else{
+        img.setAttribute('src', 'imagem/idosoM.png')
       }
-      else if (idade < 18){
-        img.src = 'imagem/adolescenteF.png'
-        res.appendChild(img)
+    }else if (fsex[1].checked){
+      genero = 'Mulher'
+      if(idade >= 0 && idade < 10){
+        img.setAttribute('src', 'imagem/criancaF.png')
+      }else if (idade < 21){
+        img.setAttribute('src', 'imagem/adolescenteF.png')
+      }else if (idade < 50){
+        img.setAttribute('src', 'imagem/adultoF.png')
+      }else{
+        img.setAttribute('src', 'imagem/idosoF.png')
       }
     }
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.appendChild(img)
   }
 }
